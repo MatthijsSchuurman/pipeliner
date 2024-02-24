b4efe41bc9e77e41eaffb4bf5e07a8f6871d528b
Matthijs Schuurman <git@matthijsschuurman.nl>
2024-02-24 11:50:16 +0100

    aligned dotnet+node build tests with jvm

--- examples/node/.pipelines/test/build.stage.sh
+++ examples/node/.pipelines/test/build.stage.sh
@@ -10,4 +10,16 @@ IntegrationTest_Examples_Node_Stage_Build() {
   #Then
   actual=$(Docker_List examples/node/app1:latest)
   Assert_Contains "$actual" "examples/node/app1" "latest"
+
+  actual=$(Docker_Run examples/node/app1:latest "" "" "node --version")
+  Assert_Match "$actual" "[0-9]+\.[0-9]+\.[0-9]+"
+
+  Docker_Run examples/node/app1:latest "" "" "test -f /home/node/app/package.json"
+  Assert_Equal $? 0
+  Docker_Run examples/node/app1:latest "" "" "test -f /home/node/app/src/main.js"
+  Assert_Equal $? 0
+  Docker_Run examples/node/app1:latest "" "" "test -d /home/node/app/node_modules/.bin"
+  Assert_Equal $? 0
+  Docker_Run examples/node/app1:latest "" "" "test -d /home/node/app/node_modules/mocha"
+  Assert_Equal $? 1
 }
