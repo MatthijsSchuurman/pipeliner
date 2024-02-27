c6c96b5a5e53ffc11030e57facf75285f6a5adf4
Matthijs Schuurman <git@matthijsschuurman.nl>
2024-02-27 16:00:27 +0100

    added basic php support

---
+++ .pipeliner/php/php.class.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source $(Files_Path_Pipeliner)/core/docker.class.sh
+
+PHP_Run() {
+  local workdir=$1
+  shift 1
+  local commands=("$@")
+
+  Docker_Runner php "$workdir" "" "${commands[@]}"
+}
+
+PHP_Build() {
+  local workdir=$1
+
+  Log_Error "PHP Build not implemented"
+  return 1
+}
+
+PHP_Test() {
+  local workdir=$1
+
+  Log_Info "PHP Test"
+  PHP_Run "$workdir" "./.composer/vendor/bin/phpunit tests"
+}
+
+PHP_Lint() {
+  local workdir=$1
+
+  Log_Info "PHP Lint"
+  PHP_Run "$workdir" "php -l ."
+}
---
+++ .pipeliner/php/test/php.class.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+source $(Files_Path_Pipeliner)/php/php.class.sh
+
+UnitTest_PHP_Dockerfile() {
+  #Given
+  local tools=(ls php composer)
+
+  #When
+  for tool in ${tools[@]}; do
+    Docker_Runner php "" "" "which $tool"
+    exitCode=$?
+
+    #Then
+    Assert_Equal $exitCode 0
+  done
+
+
+  #When
+  actual=$(Docker_Runner php "" "" "php --version")
+
+  #Then
+  Assert_Match "$actual" "PHP 8\.3\.[0-9]+"
+
+
+  #When
+  actual=$(Docker_Runner php "" "" "composer --version")
+
+  #Then
+  Assert_Match "$actual" Composer version "[0-9]+\.[0-9]+\.[0-9]+"
+
+
+  #When
+  actual=$(Docker_Runner php "" "" "/php/vendor/bin/phpunit --version")
+
+  #Then
+  Assert_Match "$actual" "PHPUnit 11\.[0-9]+\.[0-9]+"
+}
+
+UnitTest_PHP_Run() {
+  #Given
+  local actual=
+  local exitCode=
+  local workdir=examples/php
+
+  #When
+  actual=$(PHP_Run $workdir "php --version")
+  exitCode=$?
+
+  #Then
+  Assert_Equal $exitCode 0
+  Assert_Match "$actual" "PHP [0-9]+\.[0-9]+\.[0-9]+"
+
+  #When
+  actual=$(PHP_Run $workdir "composer --version" "ls -la")
+  exitCode=$?
+
+  #Then
+  Assert_Equal $exitCode 0
+  Assert_Match "$actual" Composer version "[0-9]+\.[0-9]+\.[0-9]+" examples drwx
+}
