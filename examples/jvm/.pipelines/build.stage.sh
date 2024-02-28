dcebfd114bf75e7fc7682fb50c946e35dbcb1035
Matthijs Schuurman <git@matthijsschuurman.nl>
2024-02-28 11:50:48 +0100

    aligned example build stages

--- examples/jvm/.pipelines/build.stage.sh
+++ examples/jvm/.pipelines/build.stage.sh
@@ -9,5 +9,8 @@ JVM_Pipelines_Stage_Build() {
   local curpath=$(pwd)
   cd "$(Files_Path_Root)/$app"
   Docker_Build Dockerfile "$app:latest"
+  local exitCode=$?
   cd "$curpath"
+
+  return $exitCode
 }
